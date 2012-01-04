2d4b1a21e1dbc60f29af7e9f68a0d68e3dd43335
Austin Wright <diamondmagic@users.sourceforge.net>
2012-01-04 03:34:50 -0500

    Add some Turtle parsing tests

--- test/parser-Turtle-test.js
+++ test/parser-Turtle-test.js
@@ -1,42 +1,50 @@
 var vows=require('vows');
 var assert=require('assert');
 var rdf=require('rdf');
+var env=rdf.environment;
 
-vows.describe('Turtle parsing').addBatch(
-{ "URI triple statement":
-	{ topic: ""
-	, "Subject matches": function(t){}
-	, "Predicate matches": function(t){}
-	, "Object matches": function(t){}
+
+
+
+var batches = {};
+var id=0;
+function addDocument(turtle, triples){
+	if(turtle instanceof Array) turtle=turtle.join("\n");
+	var batch = batches['#'+(++id)+': '+turtle.replace(/\n/g,"\\n ")] = { topic: function(){
+		var graph = new rdf.IndexedGraph;
+		var turtleParser = new (require('rdf/TurtleParser').Turtle)(env, undefined, undefined, graph);
+		turtleParser.parse(turtle, undefined, undefined, graph);
+		console.error(graph);
+		return graph;
+	} };
+	batch["length==="+triples.length] = function(graph){ assert.strictEqual(graph.length, triples.length); }
+	for(var i=0; i<triples.length; i++){
+		var triple = triples[i];
+		batch["Exists: "+triple.toString()] = function(graph){
+			assert.strictEqual(graph.filter({subject:triple.subject, predicate:triple.predicate, object:triple.object}).length, 1);
+		}
 	}
-, "String literal statement":
-	{ topic: ""
-	, "Subject matches": function(t){}
-	, "Predicate matches": function(t){}
-	, "Object matches": function(t){}
-	}
-, "Typed literal statement":
-	{ topic: ""
-	, "Subject matches": function(t){}
-	, "Predicate matches": function(t){}
-	, "Object matches": function(t){}
-	}
-, "RDF Collections":
-	{ topic: ""
-	, "Subject matches": function(t){}
-	, "Predicate matches": function(t){}
-	, "Object matches": function(t){}
-	}
-, "Blank Nodes square brackets":
-	{ topic: ""
-	, "Subject matches": function(t){}
-	, "Predicate matches": function(t){}
-	, "Object matches": function(t){}
-	}
-, "Blank Nodes underscore syntax":
-	{ topic: ""
-	, "Subject matches": function(t){}
-	, "Predicate matches": function(t){}
-	, "Object matches": function(t){}
-	}
-}).export(module);
+}
+
+addDocument('<http://example.com/report> a <http://example.com/Document>.',
+	[ env.createTriple("http://example.com/report", "http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "http://example.com/Document")
+	]);
+addDocument('<http://example.com/report> rdf:type <http://example.com/Document>.',
+	[ env.createTriple("http://example.com/report", "http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "http://example.com/Document")
+	]);
+addDocument('<http://example.com/report> rdf:value "22".',
+	[ env.createTriple("http://example.com/report", "http://www.w3.org/1999/02/22-rdf-syntax-ns#value", env.createLiteral("22"))
+	]);
+addDocument(
+	[ '# this is a complete turtle document'
+	, '@prefix foo: <http://example.org/ns#> .'
+	, '@prefix : <http://other.example.org/ns#> .'
+	, 'foo:bar foo: : .'
+	, ':bar : foo:bar .'
+	],
+	[ env.createTriple("http://example.org/ns#bar", "http://example.org/ns#", 'http://other.example.org/ns#')
+	, env.createTriple("http://other.example.org/ns#bar", "http://other.example.org/ns#", 'http://example.org/ns#bar')
+	, env.createTriple("http://other.example.org/ns#bar", "http://other.example.org/ns3#", 'http://example.org/ns#bar')
+	]);
+
+vows.describe('Turtle parsing').addBatch(batches).export(module);
