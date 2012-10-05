297a8097b1227144439c7124ab127ed0f5e47b0d
Austin Wright <diamondmagic@users.sourceforge.net>
2012-10-05 13:41:21 -0400

    Lots of random changes all at once with var.ref(), n3() and add tests
    Change behavior of toString/valueOf in BlankNode to align with Interfaces spec
    Add tests for .toNT()
    Move the ref() function out so anyone can call it

--- test/builtins-ref-test.js
+++ test/builtins-ref-test.js
@@ -7,7 +7,7 @@ rdf.setBuiltins();
 
 var batches = {};
 var id=0;
-function generateRefTest(subject, topic, n3out, triplesout){
+function generateRefTest(subject, topic, n3out, ntout, triplesout){
 	var n3rep = this.title;
 	var context = batches['object'+(++id)+'.ref('+util.inspect(subject)+')'] =
 		{ topic: topic.ref(subject)
@@ -18,8 +18,12 @@ function generateRefTest(subject, topic, n3out, triplesout){
 		, ".n3()":
 			{ topic: function(t){ return t.n3(); }
 			}
+		, ".toNT()":
+			{ topic: function(t){ return t.toNT(); }
+			}
 		};
-	context[".n3()"][n3out] = function(t){ assert.strictEqual(t, n3out); };
+	context[".n3()"]["Output"] = function(t){ assert.strictEqual(t, n3out); };
+	context[".toNT()"]["Output"] = function(t){ assert.strictEqual(t, ntout); };
 	var graphTest = context[".graphify()"] = { topic: function(t){ return t.graphify(); } };
 	graphTest["length==="+triplesout.length] = function(graph){ assert.strictEqual(graph.length, triplesout.length); }
 	for(var i=0; i<triplesout.length; i++){
@@ -31,27 +35,39 @@ function generateRefTest(subject, topic, n3out, triplesout){
 	return context;
 }
 
-generateRefTest('_:topic1', {a: 'rdfs:Class'}, '_:topic1 rdf:type rdfs:Class .',
+generateRefTest('_:topic1', {a: 'rdfs:Class'},
+	'_:topic1\n\trdf:type rdfs:Class .',
+	'_:topic1 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2000/01/rdf-schema#Class> .',
 	[ env.createTriple('_:topic1', "http://www.w3.org/1999/02/22-rdf-syntax-ns#type", 'http://www.w3.org/2000/01/rdf-schema#Class')
 	]);
 
-generateRefTest('_:topic2', {rdf$value: 42}, '_:topic2 rdf:value 42 .',
+generateRefTest('_:topic2', {rdf$value: 42},
+	'_:topic2\n\trdf:value 42 .',
+	'_:topic2 <http://www.w3.org/1999/02/22-rdf-syntax-ns#value> 42 .',
 	[ env.createTriple('_:topic2', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value', env.createLiteral('42',null,'http://www.w3.org/2001/XMLSchema#integer'))
 	]);
 
-generateRefTest('_:topic3', {rdf$value: "A string.".l()}, '_:topic3 rdf:value "A string." .',
+generateRefTest('_:topic3', {rdf$value: "A string.".l()},
+	'_:topic3\n\trdf:value "A string." .',
+	'_:topic3 <http://www.w3.org/1999/02/22-rdf-syntax-ns#value> "A string." .',
 	[ env.createTriple('_:topic3', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value', env.createLiteral("A string.",null,null))
 	]);
 
-generateRefTest('_:topic4', {rdf$value: env.createLiteral("A string 2000",null,null)}, '_:topic4 rdf:value "A string 2000" .',
+generateRefTest('_:topic4', {rdf$value: env.createLiteral("A string 2000",null,null)},
+	'_:topic4\n\trdf:value "A string 2000" .',
+	'_:topic4 <http://www.w3.org/1999/02/22-rdf-syntax-ns#value> "A string 2000" .',
 	[ env.createTriple('_:topic4', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value', env.createLiteral("A string 2000"))
 	]);
 
-generateRefTest('_:topic5', {rdf$value: "2".l()}, '_:topic5 rdf:value "2" .',
+generateRefTest('_:topic5', {rdf$value: "2".l()},
+	'_:topic5\n\trdf:value "2" .',
+	'_:topic5 <http://www.w3.org/1999/02/22-rdf-syntax-ns#value> "2" .',
 	[ env.createTriple('_:topic5', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value', env.createLiteral("2"))
 	]);
 
-generateRefTest('_:topic6', {rdf$value: env.createBlankNode()}, '_:topic6 rdf:value _:b1 .',
+generateRefTest('_:topic6', {rdf$value: env.createBlankNode()},
+	'_:topic6\n\trdf:value _:b1 .',
+	'_:topic6 <http://www.w3.org/1999/02/22-rdf-syntax-ns#value> _:b1 .',
 	[ env.createTriple('_:topic6', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value', '_:b1')
 	]);
 
@@ -63,8 +79,9 @@ generateRefTest('_:topic6', {rdf$value: env.createBlankNode()}, '_:topic6 rdf:va
 generateRefTest( 'dbr:Albert_Einstein',
 	{ dbp$dateOfBirth: new Date("1879-03-14")
 	, 'foaf:depiction': 'http://en.wikipedia.org/wiki/Image:Albert_Einstein_Head.jpg'
-	}
-	, 'dbr:Albert_Einstein dbp:dateOfBirth "1879-03-14"^^xsd:date;\n\tfoaf:depiction <http://en.wikipedia.org/wiki/Image:Albert_Einstein_Head.jpg> .',
+	},
+	'dbr:Albert_Einstein dbp:dateOfBirth "1879-03-14"^^xsd:date;\n\tfoaf:depiction <http://en.wikipedia.org/wiki/Image:Albert_Einstein_Head.jpg> .',
+	'dbr:Albert_Einstein dbp:dateOfBirth "1879-03-14"^^xsd:date;\n\tfoaf:depiction <http://en.wikipedia.org/wiki/Image:Albert_Einstein_Head.jpg> .',
 	[ env.createTriple("http://dbpedia.org/resource/Albert_Einstein", "http://xmlns.com/foaf/0.1/depiction", 'http://en.wikipedia.org/wiki/Image:Albert_Einstein_Head.jpg')
 	, env.createTriple("http://dbpedia.org/resource/Albert_Einstein", "http://dbpedia.org/property/dateOfBirth", env.createLiteral("1879-03-14",null,'http://www.w3.org/2001/XMLSchema#date'))
 	]);
