8dc5d2bc9fe8ba9a4f54a51ac51bad4df6ce5b2f
Austin Wright <diamondmagic@users.sourceforge.net>
2014-09-16 20:35:49 -0700

    Fix xsd: URIs in tests

--- test/context-convert-test.js
+++ test/context-convert-test.js
@@ -1,6 +1,7 @@
 var vows=require('vows');
 var assert=require('assert');
 var rdf=require('rdf');
+var rdfenv=rdf.environment;
 var util=require('util');
 
 var batches = {};
@@ -22,35 +23,35 @@ NaNCompare.nativeValue = Number.NaN;
 
 function addBatch(literal, type, test){
 	// Literal must be a string
-	var unit = batches['('+util.inspect(literal)+').t('+type+')'] =
-		{ topic: function(){ return rdf.environment.createLiteral(literal, null, type).valueOf(); }
+	var unit = batches['('+util.inspect(literal)+').t(xsd:'+type+')'] =
+		{ topic: function(){ return rdfenv.createLiteral(literal, null, rdf.xsdns(type)).valueOf(); }
 		};
 	unit['.valueOf()==='+util.inspect(test.nativeValue)] = test;
 }
 
-addBatch("A STRING", 'xsd:string', strictCompare("A STRING"));
-addBatch("true", "xsd:boolean", strictCompare(true));
-addBatch("1", "xsd:boolean", strictCompare(true));
-addBatch("false", "xsd:boolean", strictCompare(false));
-addBatch("0", "xsd:boolean", strictCompare(false));
-addBatch("0.5", "xsd:float", strictCompare(0.5));
-addBatch("INF", "xsd:float", strictCompare(Number.POSITIVE_INFINITY));
-addBatch("-INF", "xsd:float", strictCompare(Number.NEGATIVE_INFINITY));
-addBatch("NaN", "xsd:float", NaNCompare);
-addBatch("-14000", "xsd:integer", strictCompare(-14000));
-addBatch("-9223372036854775808", "xsd:long", strictCompare(-9223372036854775808));
-addBatch("9223372036854775807", "xsd:long", strictCompare(9223372036854775807));
-addBatch("1.578125", "xsd:float", strictCompare(1.578125));
-addBatch("-1.2344e56", "xsd:float", strictCompare(-1.2344e56));
-addBatch("-42", "xsd:nonPositiveInteger", strictCompare(-42));
-addBatch("0", "xsd:nonPositiveInteger", strictCompare(0));
-addBatch("42", "xsd:nonNegativeInteger", strictCompare(42));
-addBatch("0", "xsd:nonNegativeInteger", strictCompare(0));
-addBatch("-42", "xsd:negativeInteger", strictCompare(-42));
-addBatch("-2147483648", "xsd:long", strictCompare(-2147483648));
-addBatch("2147483647", "xsd:long", strictCompare(2147483647));
-addBatch("2000-01-01", "xsd:date", dateCompare(new Date('Sat, 01 Jan 2000 00:00:00 GMT')));
-//addBatch("21:32:52", "xsd:time", dateCompare(new Date('Sat, 01 Jan 2000 00:00:00 GMT'))); // There's no good way to represent just time-of-day... Ignore this guy?
-addBatch("2001-10-26T21:32:52.12679", "xsd:dateTime", dateCompare(new Date('2001-10-26T21:32:52.12679'))); // FIXME of course this is going to pass
+addBatch("A STRING", 'string', strictCompare("A STRING"));
+addBatch("true", "boolean", strictCompare(true));
+addBatch("1", "boolean", strictCompare(true));
+addBatch("false", "boolean", strictCompare(false));
+addBatch("0", "boolean", strictCompare(false));
+addBatch("0.5", "float", strictCompare(0.5));
+addBatch("INF", "float", strictCompare(Number.POSITIVE_INFINITY));
+addBatch("-INF", "float", strictCompare(Number.NEGATIVE_INFINITY));
+addBatch("NaN", "float", NaNCompare);
+addBatch("-14000", "integer", strictCompare(-14000));
+addBatch("-9223372036854775808", "long", strictCompare(-9223372036854775808));
+addBatch("9223372036854775807", "long", strictCompare(9223372036854775807));
+addBatch("1.578125", "float", strictCompare(1.578125));
+addBatch("-1.2344e56", "float", strictCompare(-1.2344e56));
+addBatch("-42", "nonPositiveInteger", strictCompare(-42));
+addBatch("0", "nonPositiveInteger", strictCompare(0));
+addBatch("42", "nonNegativeInteger", strictCompare(42));
+addBatch("0", "nonNegativeInteger", strictCompare(0));
+addBatch("-42", "negativeInteger", strictCompare(-42));
+addBatch("-2147483648", "long", strictCompare(-2147483648));
+addBatch("2147483647", "long", strictCompare(2147483647));
+addBatch("2000-01-01", "date", dateCompare(new Date('Sat, 01 Jan 2000 00:00:00 GMT')));
+//addBatch("21:32:52", "time", dateCompare(new Date('Sat, 01 Jan 2000 00:00:00 GMT'))); // There's no good way to represent just time-of-day... Ignore this guy?
+addBatch("2001-10-26T21:32:52.12679", "dateTime", dateCompare(new Date('2001-10-26T21:32:52.12679'))); // FIXME of course this is going to pass
 
 vows.describe('rdf.context.convert').addBatch(batches).export(module);
