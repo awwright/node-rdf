dd120768888eed776ca071c0a8a356406116c0a7
Austin Wright <aaa@bzfx.net>
2018-05-30 17:54:53 -0700

    Add tests for parse()

--- test/parse.test.js
+++ test/parse.test.js
@@ -3,8 +3,17 @@ var rdf = require('..');
 var env = rdf.environment;
 var util = require('util');
 
+function triple(s, p, o){
+	// Don't normally use `new rdf.BlankNode`, except where determinism in output is needed
+	return rdf.environment.createTriple(
+		typeof s=='string' ? (s.substring(0,2)=='_:' ? new rdf.BlankNode(s) : rdf.environment.createNamedNode(s)) : s ,
+		typeof p=='string' ? (p.substring(0,2)=='_:' ? new rdf.BlankNode(p) : rdf.environment.createNamedNode(p)) : p ,
+		typeof o=='string' ? (o.substring(0,2)=='_:' ? new rdf.BlankNode(o) : rdf.environment.createNamedNode(o)) : o
+	);
+}
+
 function generateRefTest(subject, topic, n3out, ntout, triplesout){
-	var t = topic.ref(subject);
+	var t = rdf.parse(topic, subject);
 	assert.equal(typeof t.n3, 'function');
 	assert.equal(typeof t.toNT, 'function');
 	assert.equal(typeof t.graphify, 'function');
@@ -20,59 +29,52 @@ function generateRefTest(subject, topic, n3out, ntout, triplesout){
 }
 
 describe('Object builtins', function(){
-	before(function(){
-		rdf.setBuiltins();
-	});
-	after(function(){
-		rdf.unsetBuiltins();
-	});
-
-	it('(_:topic1).ref', function(){
+	it('parse(_:topic1)', function(){
 		generateRefTest('_:topic1', {a: 'rdfs:Class'},
 			'_:topic1\n\trdf:type rdfs:Class .',
 			'_:topic1 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2000/01/rdf-schema#Class> .',
-			[ env.createTriple('_:topic1', "http://www.w3.org/1999/02/22-rdf-syntax-ns#type", 'http://www.w3.org/2000/01/rdf-schema#Class')
+			[ triple('_:topic1', "http://www.w3.org/1999/02/22-rdf-syntax-ns#type", 'http://www.w3.org/2000/01/rdf-schema#Class')
 			]);
 	});
-	it('(_:topic2).ref', function(){
+	it('parse(_:topic2)', function(){
 		generateRefTest('_:topic2', {rdf$value: 42},
 			'_:topic2\n\trdf:value 42 .',
 			'_:topic2 <http://www.w3.org/1999/02/22-rdf-syntax-ns#value> "42"^^<http://www.w3.org/2001/XMLSchema#double> .',
-			[ env.createTriple('_:topic2', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value', env.createLiteral('42',null,'http://www.w3.org/2001/XMLSchema#integer'))
+			[ triple('_:topic2', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value', env.createLiteral('42',null,'http://www.w3.org/2001/XMLSchema#integer'))
 			]);
 	});
-	it('(_:topic3).ref', function(){
+	it('parse(_:topic3)', function(){
 		generateRefTest('_:topic3', {rdf$value: "A string.".l()},
 			'_:topic3\n\trdf:value "A string." .',
 			'_:topic3 <http://www.w3.org/1999/02/22-rdf-syntax-ns#value> "A string." .',
-			[ env.createTriple('_:topic3', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value', env.createLiteral("A string.",null,null))
+			[ triple('_:topic3', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value', env.createLiteral("A string.",null,null))
 			]);
 	});
-	it('(_:topic4).ref', function(){
+	it('parse(_:topic4)', function(){
 		generateRefTest('_:topic4', {rdf$value: env.createLiteral("A string 2000",null,null)},
 			'_:topic4\n\trdf:value "A string 2000" .',
 			'_:topic4 <http://www.w3.org/1999/02/22-rdf-syntax-ns#value> "A string 2000" .',
-			[ env.createTriple('_:topic4', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value', env.createLiteral("A string 2000"))
+			[ triple('_:topic4', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value', env.createLiteral("A string 2000"))
 			]);
 	});
-	it('(_:topic5).ref', function(){
+	it('parse(_:topic5)', function(){
 		generateRefTest('_:topic5', {rdf$value: "2".l()},
 			'_:topic5\n\trdf:value "2" .',
 			'_:topic5 <http://www.w3.org/1999/02/22-rdf-syntax-ns#value> "2" .',
-			[ env.createTriple('_:topic5', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value', env.createLiteral("2"))
+			[ triple('_:topic5', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value', env.createLiteral("2"))
 			]
 		);
 	});
-	it('(_:topic6).ref', function(){
+	it('parse(_:topic6)', function(){
 		// Normally, use env.createBlankNode
 		// For deterministic tests, we use BlankNode with a name
 		generateRefTest('_:topic6', {rdf$value: new rdf.BlankNode('_:b1')},
 			'_:topic6\n\trdf:value _:b1 .',
 			'_:topic6 <http://www.w3.org/1999/02/22-rdf-syntax-ns#value> _:b1 .',
-			[ env.createTriple('_:topic6', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value', '_:b1')
+			[ triple('_:topic6', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value', '_:b1')
 			]);
 	});
-	it('(dbr:Albert_Einstein).ref', function(){
+	it('parse(dbr:Albert_Einstein)', function(){
 		/*
 			dbp:dateOfBirth "1879-03-14"^^xsd:date ;
 			foaf:depiction <http://en.wikipedia.org/wiki/Image:Albert_Einstein_Head.jpg> .
@@ -84,8 +86,8 @@ describe('Object builtins', function(){
 			},
 			'dbr:Albert_Einstein dbp:dateOfBirth "1879-03-14"^^xsd:date;\n\tfoaf:depiction <http://en.wikipedia.org/wiki/Image:Albert_Einstein_Head.jpg> .',
 			'<http://dbpedia.org/resource/Albert_Einstein> <http://dbpedia.org/property/dateOfBirth> "1879-03-14T00:00:00Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .\n<http://dbpedia.org/resource/Albert_Einstein> <http://xmlns.com/foaf/0.1/depiction> <http://en.wikipedia.org/wiki/Image:Albert_Einstein_Head.jpg> .',
-			[ env.createTriple("http://dbpedia.org/resource/Albert_Einstein", "http://xmlns.com/foaf/0.1/depiction", 'http://en.wikipedia.org/wiki/Image:Albert_Einstein_Head.jpg')
-			, env.createTriple("http://dbpedia.org/resource/Albert_Einstein", "http://dbpedia.org/property/dateOfBirth", env.createLiteral("1879-03-14",null,'http://www.w3.org/2001/XMLSchema#date'))
+			[ triple("http://dbpedia.org/resource/Albert_Einstein", "http://xmlns.com/foaf/0.1/depiction", 'http://en.wikipedia.org/wiki/Image:Albert_Einstein_Head.jpg')
+			, triple("http://dbpedia.org/resource/Albert_Einstein", "http://dbpedia.org/property/dateOfBirth", env.createLiteral("1879-03-14",null,'http://www.w3.org/2001/XMLSchema#date'))
 			]);
 	});
 });
