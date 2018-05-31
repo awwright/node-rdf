767fec161997c6c70e639b7ef22818830a50a57f
Austin Wright <aaa@bzfx.net>
2018-05-31 02:20:41 -0700

    Add eval Turtle test suite

--- test/TurtleParser.test.js
+++ test/TurtleParser.test.js
@@ -4,13 +4,16 @@ var rdf = require('..');
 var env = rdf.environment;
 var TurtleParser = rdf.TurtleParser;
 
+var m$ = rdf.ns('http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#');
+
 var manifestBase = 'http://www.w3.org/2013/TurtleTests/manifest.ttl';
+//var manifestBase = 'file://'+__dirname+'/TurtleTests/manifest.ttl';
 var manifestData = require('fs').readFileSync('test/TurtleTests/manifest.ttl');
 var manifestGraph = env.createGraph();
 var manifestParse = new TurtleParser;
 manifestParse.parse(manifestData, undefined, manifestBase, null, manifestGraph);
 
-var manifest = manifestGraph.match(manifestBase, 'http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#entries');
+var manifest = manifestGraph.match(manifestBase, m$('entries'));
 var manifestTests = manifestGraph.getCollection(manifest[0].object);
 
 
@@ -22,13 +25,37 @@ describe('Turtle test suite', function(){
 
 		manifestTests.forEach(function(test){
 			var types = manifestGraph.match(test, rdf.rdfns('type')).map(function(v){return v.object});
-
-			var file = manifestGraph.match(test, 'http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#action').map(function(v){return v.object})[0];
-			var filename = file.toString().replace('http://www.w3.org/2013/','test/');
+			var fileURI = manifestGraph.match(test, m$('action')).map(function(v){return v.object.toString();})[0];
+			var filename = fileURI.replace('http://www.w3.org/2013/', 'test/');
 
 			for(var j=0; j<types.length; j++){
 				switch(types[j].toString()){
 					case 'http://www.w3.org/ns/rdftest#TestTurtleEval':
+						it('evaluation <'+filename+'>', function(done){
+							// First parse the expected data (in N-Triples format)
+							var resultURI = manifestGraph.match(test, m$('result')).map(function(v){return v.object.toString()})[0];
+							var resultFile = resultURI.replace('http://www.w3.org/2013/', 'test/');
+							fs.readFile(resultFile, 'utf8', function(err, data){
+								if(err) throw err;
+								var expectedGraph = env.createGraph();
+								var expectedParser = new TurtleParser;
+								expectedParser.parse(data, undefined, resultURI, null, expectedGraph);
+								// Now parse the Turtle file
+								fs.readFile(filename, 'utf8', function(err, data){
+									if(err) throw err;
+									var graph = env.createGraph();
+									var parser = new TurtleParser;
+									parser.parse(data, undefined, fileURI, null, graph);
+									// Have the data, compare
+									var expectedTriples = expectedGraph.toArray().sort();
+									var outputTriples = graph.toArray().sort();
+
+									//assert.ok(expectedGraph.equals(graph));
+									assert.equal(expectedTriples.join('\n'), outputTriples.join('\n'));
+									done();
+								});
+							});
+						});
 						break;
 					case 'http://www.w3.org/ns/rdftest#TestTurtleNegativeEval':
 						break;
@@ -38,7 +65,7 @@ describe('Turtle test suite', function(){
 								if(err) throw err;
 								var graph = env.createGraph();
 								var parser = new TurtleParser;
-								parser.parse(data, undefined, file, null, graph);
+								parser.parse(data, undefined, fileURI, null, graph);
 								done();
 							});
 						});
