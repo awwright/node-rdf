bc7ba0a2dba0dc62b6e83d694c4587f2c47eef61
Austin Wright <aaa@bzfx.net>
2018-06-01 14:54:26 -0700

    Move valueOf tests into Literal.test.js

--- test/Literal.test.js
+++ test/Literal.test.js
@@ -110,4 +110,50 @@ describe('Literal (with builtins)', function(){
 		assert.ok(!t.equals(new rdf.Literal('1', 'http://www.w3.org/2001/XMLSchema#integer')));
 		assert.ok(!t.equals(new rdf.Literal('123', 'http://www.w3.org/2001/XMLSchema#decimal')));
 	});
+	describe("valueOf", function(){
+		testValueOf("A STRING", 'string', "A STRING");
+		testValueOf("true", "boolean", true);
+		testValueOf("1", "boolean", true);
+		testValueOf("false", "boolean", false);
+		testValueOf("0", "boolean", false);
+		testValueOf("0.5", "float", 0.5);
+		testValueOf("INF", "float", Number.POSITIVE_INFINITY);
+		testValueOf("-INF", "float", Number.NEGATIVE_INFINITY);
+		testValueOf("NaN", "float", 0/0);
+		testValueOf("-14000", "integer", -14000);
+		testValueOf("-9223372036854775808", "long", -9223372036854775808);
+		testValueOf("9223372036854775807", "long", 9223372036854775807);
+		testValueOf("1.578125", "float", 1.578125);
+		testValueOf("-1.2344e56", "float", -1.2344e56);
+		testValueOf("-42", "nonPositiveInteger", -42);
+		testValueOf("0", "nonPositiveInteger", 0);
+		testValueOf("42", "nonNegativeInteger", 42);
+		testValueOf("0", "nonNegativeInteger", 0);
+		testValueOf("-42", "negativeInteger", -42);
+		testValueOf("-2147483648", "long", -2147483648);
+		testValueOf("2147483647", "long", 2147483647);
+		testValueOf("2000-01-01", "date", new Date('Sat, 01 Jan 2000 00:00:00 GMT'));
+		//testValueOf("21:32:52", "time", new Date('Sat, 01 Jan 2000 00:00:00 GMT')); // There's no good way to represent just time-of-day... Ignore this guy?
+		testValueOf("2001-10-26T21:32:52.12679", "dateTime", new Date('2001-10-26T21:32:52.12679'));
+		// TODO probbly should verify the inputs are of the correct range?
+	});
 });
+
+var assert = require('assert');
+var rdf = require('..');
+var rdfenv = rdf.environment;
+
+function testValueOf(literal, type, test){
+	// Literal must be a string
+	it('Literal('+JSON.stringify(literal)+', xsd:'+type+').valueOf()', function(){
+		var value = new rdf.Literal(literal, rdf.xsdns(type)).valueOf();
+		if(type=='date' || type=='dateTime'){
+			assert.ok(value instanceof Date);
+			assert.strictEqual(value.getTime(), test.getTime());
+		}else if(Number.isNaN(test)){
+			assert.ok(Number.isNaN(value));
+		}else{
+			assert.strictEqual(value, test);
+		}
+	});
+}
